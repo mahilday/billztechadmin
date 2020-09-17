d4878738cdc585d26d216936fc340335ff36f0a5
Mathilda Imadojiemu <mathildaimadojiemu@gmail.com>
2020-09-17 07:04:16 +0100

    updates

--- src/app/nurse/bookings/bookings.component.ts
+++ src/app/nurse/bookings/bookings.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import {GetDataService} from '../../services/getdata.service'
+import { HttpClient } from '@angular/common/http'
 import {Router} from '@angular/router'
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-bookings',
@@ -10,14 +12,28 @@ import {Router} from '@angular/router'
 })
 export class BookingsComponent implements OnInit {
 
-  constructor(private _auth: AuthService, private router: Router, private getdata: GetDataService) { }
+  constructor(private _auth: AuthService, private _http:HttpClient, private router: Router, private getdata: GetDataService) { }
 
   ngOnInit(): void {
+    this.myself()
   }
   nurse = this._auth.loginDets
+  bookings =[]
   date(data){
     let f = new Date(data).toLocaleString()
     return f
+  }
+  myself(){
+    this._http
+    .post(`${environment.baseUrl}/getvacc`, this.nurse)
+    .toPromise()
+    .then((res: any)=>{
+      console.log('done')
+      this.bookings = res.result
+    })
+    .catch(()=>{
+      console.log('not done')
+    })
   }
   formEdit=(data)=>{
     this.router.navigateByUrl(`edit_booking/${data._id}`)
--- src/app/nurse/eachbooking/eachbooking.component.ts
+++ src/app/nurse/eachbooking/eachbooking.component.ts
@@ -1,22 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import {GetDataService} from '../../services/getdata.service'
-
+import { Component, OnInit } from "@angular/core";
+import { GetDataService } from "../../services/getdata.service";
 
 @Component({
-  selector: 'app-eachbooking',
-  templateUrl: './eachbooking.component.html',
-  styleUrls: ['./eachbooking.component.scss']
+  selector: "app-eachbooking",
+  templateUrl: "./eachbooking.component.html",
+  styleUrls: ["./eachbooking.component.scss"],
 })
 export class EachbookingComponent implements OnInit {
-
-  constructor(private getdata: GetDataService) { }
+  constructor(private getdata: GetDataService) {}
 
   ngOnInit(): void {
+    this.returnStages()
   }
-  date(data){
-    let f = new Date(data).toLocaleString()
-    return f
+  date(data) {
+    let f = new Date(data).toLocaleString();
+    return f;
   }
-  booking = this.getdata.bookingedit
 
+  returnStages(){
+    for(let i =0; i<40; i++){
+      this.stages.push(i)
+    }
+  }
+  newStages =[]
+  changeDosage() {
+    this.stages.length = this.booking.dosageNumber;
+    console.log(this.stages);
+    this.newStages =[...this.stages]
+
+  }
+  allStages(i) {
+    for(let stage =0; stage < this.booking.dosageNumber; stage++){
+      if(i === (this.booking.dosageNumber - 1)){
+        return 'final stage'
+      } else{
+        return `stage ${i + 1}`;
+      }
+    }
+  }
+  booking: any = this.getdata.bookingedit;
+  stages = [];
 }
